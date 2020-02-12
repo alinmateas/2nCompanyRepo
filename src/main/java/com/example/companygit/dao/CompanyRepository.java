1153d54970b189f46244ddbed470392e6d25e14b
Alin Mateas <alinfmateas@gmail.com>
2020-02-12 13:33:50 +0200

    added repo & company pojo & companydto

---
+++ src/main/java/com/example/companygit/dao/CompanyRepository.java
@@ -0,0 +1,7 @@
+package com.example.companygit.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity,Long> {
+
+}
